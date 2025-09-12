8497d60287e26100fc70fea99a8a4746c7d11e5d
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-12 20:23:27 -0300

    att
    booleans: comparações, operadores lógicos e ternários

---
+++ exercicios/07_boolean-comparacoes/javascript/javascript.js
@@ -0,0 +1,13 @@
+console.log(1 > 2);
+
+console.log(5 < 100);
+
+console.log(3 >= 3);
+
+console.log(5 <= 4);
+
+console.log('Bruno Silvestre' != 'Bruno Silvestre');
+
+console.log(3 === '3');
+
+console.log(4 === 4);
---
+++ exercicios/08_booleans-operadores-logicos/javascript/javascript.js
@@ -0,0 +1,12 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(3 == 3 && "Bruno Silvestre" == "Bruno Silvestre");
+console.log("Miriã" == "Carla" || false);
+
+console.log(!(true && true));
+
+console.log(true && true);
+console.log(false && false)
+
+console.log(true === true);
