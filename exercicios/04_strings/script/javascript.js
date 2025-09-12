0789317183976e49dd619744a37abfe152174d18
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-12 19:21:31 -0300

    att
    att Strings e Detalhes de Strings, imprimindo uma concatenacao

---
+++ exercicios/04_strings/script/javascript.js
@@ -0,0 +1,5 @@
+console.log(typeof "Oi, meu nome é Bruno Silvestre");
+console.log(typeof 'Eu queria comprar um carro.');
+console.log(typeof `Teste, Som, Teste, Teste, Ei, Som, Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ exercicios/05_string-avancado/javascript/javascript.js
@@ -0,0 +1,6 @@
+console.log("Primeira linha\nSegunda Linha");
+console.log("O meu nome é 'Bruno Silvestre' ");
+console.log('O nome dela é "Monique" ');
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O " + " meu " + " nome " + " é " + " Bruno Silvestre ");
+console.log("A frase acima é uma concatenação");
