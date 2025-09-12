e926d67c9407ae0818ffb68b846a69fa0b8b87f7
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-12 20:26:54 -0300

    att
    att Empty Values

--- exercicios/09_booleans-operador-ternario/javascript/javascript.js
+++ exercicios/09_booleans-operador-ternario/javascript/javascript.js
@@ -3,7 +3,3 @@ console.log(5 > 2 ? 'É Sim' : 'É não');
 console.log(false ? 5 : 4);
 
 console.log("Bruno Silvestre" == "Bruno Silvestre" ? "Olá, Bruno Silvestre" : "Não é o Bruno Silvestre");
-
-console.log();
-
-console.log();
