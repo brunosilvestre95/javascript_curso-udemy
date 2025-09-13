d7c4f4947ec9791585944c10bcc5c42ef358c661
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-13 19:56:11 -0300

    att 06
    att O Ambiente do Javascript

---
+++ exercicios/04_secao/exercicios/06_o-ambiente-do-javascript/javascript/javascript.js
@@ -0,0 +1,15 @@
+/* 
+
+    Como funciona o ambiente:
+
+        quando qualquer programa é iniciado, um ambiente é criado;
+
+        e este ambiente nao inicia vazio;
+
+        ele contem Funcoes;
+
+        Funcoes sao blocos de codigo, que nos retornam um valor ou acao;
+
+        o ambiente de JS, neste curso, poderiamos entender como o navegador;
+
+*/
