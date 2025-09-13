25d79d1b4e07d5e51cb1bcaf2b74bd1871c7a90b
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-13 19:43:16 -0300

    att - 04
    att Convencao de Variaveis

---
+++ exercicios/04_secao/exercicios/03_outras-formas-de-variaveis/javascript/javascript.js
@@ -0,0 +1,9 @@
+
+var nome = "Bruno Silvestre"
+const ip = "127.0.0.1"
+
+console.log(nome);
+console.log(ip);
+
+// CONST  é uma variavel que nao pode ser alterada.
+//CONST vem de CONSTANTE.
---
+++ exercicios/04_secao/exercicios/04_convencao-de-nome-das-variaveis/javascript/javascript.js
@@ -0,0 +1,43 @@
+/*
+    nomes das variaveis nao podem iniciar com numeros
+        exemplo: let 2teste     ;
+
+    nomes das variaveis podem terminar com numeros
+        exemplo: let teste9 = 'testando';
+
+    nomes de variaveis podem comecar com $ ou _, mas nao outras caracteres.
+        exeplo: let $nome, _nome;
+
+    mas nao podem ter pontuacao, ou outros especiais
+        exemplo: let @teste      ;
+
+    podem iniciar com letra maiuscula
+        exemplo: let Nome = 'Silvestre' ;
+
+    ou usar camelCase
+        exemplo: let meuPrimeiroNome = "Bruno";
+*/
+
+//let 3nome = 'teste0';
+// let @nome = 'teste2';
+// let !nome = 'teste3';
+
+let nome3 = 'teste1';
+    console.log(nome3);
+
+let $nome = 'Silvestre';
+    console.log($nome);
+
+let _nome = 'bruno';
+    console.log(_nome);
+
+
+let Nome = 'teste Mome';
+    console.log(Nome);
+
+let seuNome= 'teste Seu Nome';
+    console.log(seuNome);
+
+let seu_nome = 'teste seu_nome';
+    console.log(seu_nome);
+
