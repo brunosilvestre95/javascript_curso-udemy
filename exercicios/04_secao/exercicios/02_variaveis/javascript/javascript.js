32c8ee095308c227fc66c1194af9e5f245f889cd
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-13 19:25:45 -0300

    att_secao-04
    att variaveis

---
+++ exercicios/04_secao/exercicios/02_variaveis/javascript/javascript.js
@@ -0,0 +1,21 @@
+
+let nome = "Bruno Silvestre";
+    console.log(nome);
+        console.log(`Olá, Mundo! O meu nome é ${nome}`);
+
+let laranjas = 5;
+    console.log(laranjas * laranjas);
+
+nome = "Silvestre";
+    console.log(nome);
+
+laranja = 18563
+    console.log(laranja);
+laranja = "Laranja";
+    console.log(laranja);
+
+let um = 1, dois = 2, tres = 3;
+    console.log(um, dois, tres);
+    console.log(um + dois + tres);
+    console.log(um - dois - tres);
+    console.log(um - dois + tres - um);
