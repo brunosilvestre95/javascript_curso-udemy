3066c539f91a1dbd99a3b6a6c484e59cfe6911dd
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-13 19:51:41 -0300

    att
    att Nomes de Variavies Reservados

---
+++ exercicios/04_secao/exercicios/05_variaveis-nomes-reservados/javascript/javascript.js
@@ -0,0 +1,16 @@
+/*  Nomes de variavies reservados
+        alguns nomes nao podem ser utilizados para iniciar variavies
+        break case cath class const continue debugger default delete do else enum explort extends flase finally for function if implements public return static super switch tis throw true try typeof var void while with yield...
+            exemplo: let break = 'parar';
+        porem podemos usar elas + alguma palavra/digito
+            exemplo: let breakSilvestre = 1;
+*/
+
+let breakSilvestre = 'Parar Silvestre';
+    console.log(breakSilvestre);
+
+let functionTest = 'Funcao Teste';
+    console.log(functionTest);
+
+let ifSe = 'se Se';
+    console.log('ifSe');
