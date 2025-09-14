0e745e5a0bfec1dc0a950e53d39492efb1a5b159
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-14 00:53:37 -0300

    att
    att Identação

---
+++ exercicios/04_secao/exercicios/19_identacao/javascript/javascript.js
@@ -0,0 +1,45 @@
+/* 
+    Sobre Identacao:
+
+        organiza o codigo e deixa mais legivel;
+
+        nao ha uma regra, porem cada bloco alinhado deve ser identado uma vez;
+        
+        execucao do codigo nao afeta em nada;
+
+            exemplo:
+                let x = 2;
+                let y = 0;
+
+                    if (x > 2) {
+                        y = x + 2;
+                        if (y == 4) {
+                            console.log('Y é 4');
+                            } 
+                    }
+*/
+
+let x = 0;
+let y = 0;
+
+    if(x == 0 && y == 0){
+
+        let z = 5;
+
+        if (z > 2) {
+
+            console.log(z);
+
+            for ( i = 0; i < 5; i = i + i) {
+
+                console.log(i);
+
+                if(i == 2){
+
+                    console.log("i é = 2");
+                }
+            }
+        }
+    }
+
+
