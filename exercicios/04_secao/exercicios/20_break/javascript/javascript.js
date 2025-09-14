61fa1022e6ca0abf073a29e3f2b8e6eb5d7d81d8
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-14 02:16:47 -0300

    att

--- exercicios/04_secao/exercicios/20_break/javascript/javascript.js
+++ exercicios/04_secao/exercicios/20_break/javascript/javascript.js
@@ -1,4 +1,17 @@
+/* forcando a saida de um loop: Break
 
+        as vezes precisamos parar o loop antes que complete todo o seu ciclo;
+
+        para isso utilizamos Break;
+
+        for(let i = 5; i < 20; i = i + 1){
+            if(i % 10 == 0) {
+                console.log('Saiu do loop);
+                break;
+            }
+                console.log('Prosseguind o loop');
+        }
+*/
     for(let i = 0; i < 10; i = i + 1){
         //let nome = "Silvestre"; // loop infinito
         
