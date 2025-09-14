283465578db7fef01b01677dac7902e38096613e
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-13 21:34:43 -0300

    att
    att Estrutura Condicional: Else

---
+++ exercicios/04_secao/exercicios/14_estrutura-condicional-else/javascript/javascript.js
@@ -0,0 +1,22 @@
+/*  
+    Estrutura condicional: else
+
+        podemos executar um outro bloco de código, caso a instrucao do If nao seja atendida;
+
+        ou seja, SE NAO EXECUTAR o bloco If, EXECUTE o bloco Else;
+
+            exemplo:
+                let nome = 'Toicin'
+                    if(nome == 'Pedro'){
+                    console.log('Seu nome é Toicin.');
+                    } else { 
+                        console.log('O seu nome não é Pedro, seu nome é Toicin.);
+                        }
+*/
+
+let velocidade = 100;
+    if(velocidade <= 60){
+        console.log("Não foi multado.");
+    } else {
+        console.log("Foi Multado!")
+    }
