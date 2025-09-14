0ad82aab65233ac33269794aa20a6dff8f01ce3e
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-14 02:05:08 -0300

    att
    att Break

---
+++ exercicios/04_secao/exercicios/20_break/javascript/javascript.js
@@ -0,0 +1,16 @@
+
+    for(let i = 0; i < 10; i = i + 1){
+        //let nome = "Silvestre"; // loop infinito
+        
+        if(i == 3){
+            nome = "Bruno";
+        }
+            if(i == 5 && nome == "Bruno") {
+
+                console.log("O nome é Bruno, pode parar.");
+                break;
+            }
+
+                console.log(i);
+
+    }
