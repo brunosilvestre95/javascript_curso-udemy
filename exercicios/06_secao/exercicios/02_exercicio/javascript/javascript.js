cf1256019f96d29538e9389385e3657f53884e72
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-16 21:16:36 -0300

    att
    att
        mais sobre funcoes;

--- exercicios/06_secao/exercicios/02_exercicio/javascript/javascript.js
+++ exercicios/06_secao/exercicios/02_exercicio/javascript/javascript.js
@@ -11,4 +11,23 @@
             }
                 console.log(saudacao("Mansur"));
     */
-   
+    console.log(multiplicarTresNumeros(2,3,4));
+
+    const mult = multiplicarTresNumeros(5, 4, 8);
+
+    console.log("O valor de mult é " + mult);
+
+
+    function podeDirigir(idade, cnh){
+        if(idade >= 18 && cnh == true){
+            console.log("Pode Dirigir.");
+        } else {
+            console.log("Não pode dirigir.");
+        }
+    }
+
+    console.log(podeDirigir(19,true));
+    console.log(podeDirigir(25,true));
+    console.log(podeDirigir(17,false));
+    console.log(podeDirigir(44, 0));
+    console.log(podeDirigir(19,1));
