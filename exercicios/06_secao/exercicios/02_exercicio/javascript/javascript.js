1c94b586c9d2b2a2c9c8f85335098032bf0a254e
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-16 21:00:36 -0300

    at

---
+++ exercicios/06_secao/exercicios/02_exercicio/javascript/javascript.js
@@ -0,0 +1,14 @@
+    /* Mais Sobre Funcoes:
+            const soma = function( a , b ) {
+                return a + b ; 
+            }
+                console.log(soma(3, 5));
+
+            const saudacao = function(nome){
+                if(nome == "Mansur"){
+                    return "Olá, Mansur!!";
+            }
+            }
+                console.log(saudacao("Mansur"));
+    */
+   
