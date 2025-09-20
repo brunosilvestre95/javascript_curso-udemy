b254249e0a834ef45578d01243e56fa048024d18
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-20 19:11:47 -0300

    Update javascript.js

--- exercicios/06_secao/exercicios/11_exercicio/javascript/javascript.js
+++ exercicios/06_secao/exercicios/11_exercicio/javascript/javascript.js
@@ -21,14 +21,23 @@
 */
 
 function recursao(n){
+    
     if(n - 1 == 2){
+    
         console.log("recursão parou.");
+    
     } else if ( n % 2 != 0){
+    
         console.log("Número ímpar" + n);
+    
         recursao(n - 1);
+    
     } else {
+    
         console.log("Numero par" + n);
+    
         recursao(n - 2);
+    
     }
 }
     recursao(39);
