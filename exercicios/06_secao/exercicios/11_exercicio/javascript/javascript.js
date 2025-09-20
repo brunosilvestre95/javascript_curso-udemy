5b2063e5dda5637ac4bbe01afd06cd56fb6c5a8a
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-20 19:09:44 -0300

    att
    att
         recursao;

--- exercicios/06_secao/exercicios/11_exercicio/javascript/javascript.js
+++ exercicios/06_secao/exercicios/11_exercicio/javascript/javascript.js
@@ -5,7 +5,32 @@
 
             por exemplo:
                 function returnarNumeroPar(n){
-                
-                
+
+                    if(n % 2 == 0){
+
+                    console.log("n agora é par: " + n);
+
+                    } else {
+
+                        console.log(n);
+
+                        retornarNumeroPar(n - 1);
+                        }
                 }
-        */
+                retornarNumeroPar(33);
+*/
+
+function recursao(n){
+    if(n - 1 == 2){
+        console.log("recursão parou.");
+    } else if ( n % 2 != 0){
+        console.log("Número ímpar" + n);
+        recursao(n - 1);
+    } else {
+        console.log("Numero par" + n);
+        recursao(n - 2);
+    }
+}
+    recursao(39);
+    recursao(10);
+    recursao(55);
