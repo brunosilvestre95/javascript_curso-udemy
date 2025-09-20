01082cb156e08241937d0be239bcdbe60c77c163
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-20 19:05:08 -0300

    Update javascript.js

---
+++ exercicios/06_secao/exercicios/11_exercicio/javascript/javascript.js
@@ -0,0 +1,11 @@
+/*  Recursion:
+        uma funcionalidade que permite uma funcao se chamar novamente;
+
+        isso pode ser um problema caso a funcao se chame muitas vezes;
+
+            por exemplo:
+                function returnarNumeroPar(n){
+                
+                
+                }
+        */
