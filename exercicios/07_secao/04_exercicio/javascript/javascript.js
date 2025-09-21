10449bb433332be7b1d5a5b3587a1a0e369a12f4
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-21 13:24:42 -0300

    att
    att
        praticando exercícios;

---
+++ exercicios/07_secao/04_exercicio/javascript/javascript.js
@@ -0,0 +1,16 @@
+/* 
+    exercício 04
+        escreva uma funcao que retorne um número aleatório;
+
+        o número máximo retornado deve ser passo via parâmetro;
+
+        dica: utiliza Math.random();
+*/
+
+    function numeroAleatorio01(num){
+        return Math.floor(Math.random() * num) + 1;
+    }
+    console.log(numeroAleatorio01(10));
+    console.log(numeroAleatorio01(100));
+    console.log(numeroAleatorio01(50));
+    console.log(numeroAleatorio01(250));
