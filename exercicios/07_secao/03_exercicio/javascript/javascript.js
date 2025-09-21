62a448fa6bd91989a77afdeb09ad23baa00d6855
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-21 12:55:31 -0300

    att
    att
         praticando exercícios;

---
+++ exercicios/07_secao/03_exercicio/javascript/javascript.js
@@ -0,0 +1,15 @@
+/* 
+    exercicio 03
+        escreva uma funcao que some dois numeros e retorne eles;
+        depois imprima este retorno;
+*/
+
+function somaDoisNumeros(a,b){
+    return(a + b);
+}
+
+somaDoisNumeros(10,20);
+somaDoisNumeros(30,50);
+somaDoisNumeros(95,120);
+somaDoisNumeros(350,730);
+somaDoisNumeros(1325,595);
