f53013e658d766225347e3b887e3cdc0048ca6ac
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-23 11:31:18 -0300

    att
    att
        praticando exercícios extras

---
+++ exercicios/07_secao/011_exercicio/javascript/javascript.js
@@ -0,0 +1,20 @@
+/*
+Função de soma
+Crie uma função chamada soma que aceita três números como argumentos;
+
+Retorne a soma dos três números;
+
+Crie uma variável chamada resultado;
+
+Ela deve conter o retorno da função, passe os números 5, 10 e 15 como argumento;
+*/
+
+function soma(a, b, c) {
+    
+  return a + b + c;
+
+}
+
+let resultado = soma(5, 10, 15);
+
+console.log(resultado);
